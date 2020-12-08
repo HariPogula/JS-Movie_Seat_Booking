02003d6cab387939cabb79b65d29ea0670fbf4b5
Hari <hariar2.0@outlook.com>
2020-12-08 15:24:39 -0700

    Store in local storage

--- script.js
+++ script.js
@@ -4,8 +4,34 @@ const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
+populateUI();
 let ticketPrice = +movieSelect.value;
 console.log(ticketPrice);
+
+//Det data from localstorage and popute in UI
+function populateUI() {
+  const selectedSeat_L = JSON.parse(localStorage.getItem("selectedSeats"));
+  console.log("Local Storage selected seats " + selectedSeat_L);
+  if (selectedSeat_L !== null && selectedSeat_L.length > 0) {
+    seats.forEach((seat, index) => {
+      // If the selected index is there
+      if (selectedSeat_L.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedMovieIndex_L = localStorage.getItem("selectedMovieIndex");
+  if (selectedMovieIndex_L !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex_L;
+  }
+}
+
+//Save Selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
 
 // To Update Total and COunt
 function updateSelectedCount() {
@@ -15,11 +41,20 @@ function updateSelectedCount() {
   console.log(selectedSeats + " and " + selectedSeatsCount);
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
+
+  //Save in local storage
+  const seatsIndex = [...selectedSeats].map((seat) => {
+    return [...seats].indexOf(seat);
+  });
+  console.log("SeatsIndex " + seatsIndex);
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
 }
 
 //Movie Select Event
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = +e.target.value;
+  console.log("SelectedMovie Index" + e.target.selectedIndex);
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
@@ -32,3 +67,6 @@ container.addEventListener("click", (e) => {
     updateSelectedCount();
   }
 });
+
+//Intial cont and total
+updateSelectedCount();
